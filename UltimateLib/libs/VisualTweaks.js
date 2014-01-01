19a9ba860ae520e2613c391e5cbdbe37806e52de
Sir_Everard <deadlysin4@gmail.com>
2013-12-31 19:24:52 -0500

    Added some basic parameters to functions

--- UltimateLib/libs/VisualTweaks.js
+++ UltimateLib/libs/VisualTweaks.js
@@ -8,6 +8,7 @@
 UltimateLib.VisualTweaks = (function(self) {    
     // Show up in console
     UltimateLib.Logger.log("UltimateLib.VisualTweaks loading...");
+    var store = GDT.getDataStore("UltimateLib");
     
     /**
      * @description Sets up the style tags for the rest of the module.
@@ -21,37 +22,52 @@ UltimateLib.VisualTweaks = (function(self) {
     /**
      * @description Give windows rounded edges.
      * @public
+     * @param {radius} Rounded edge radius on the window.
     */ 
-	 self.setRoundedWindows = function(){
+	 self.setRoundedWindows = function(radius){
 		if (store.settings.roundedCorners === false){ UltimateLib.Logger.log("UltimateLib.VisualTweaks.setRoundedWindows = false."); return; };
-		var tweak = $('#visualTweaks');
-		tweak.append('.windowBorder { border-radius: 15px !important; }');
-		UltimateLib.Logger.log("UltimateLib.VisualTweaks.setRoundedWindows set."); 
+		if (!(radius >= 0)) { radius = 15;}
+			var tweak = $('#visualTweaks');
+			tweak.append('.windowBorder { border-radius: ' + radius + 'px !important; }');
+			UltimateLib.Logger.log("UltimateLib.VisualTweaks.setRoundedWindows set."); 
 	};
 	
     /**
      * @description Adds a style to the overflow scrollbar
      * @public
+     * @param {scrollbar style} 1 = default scrollbar styles
     */ 
-	self.setScrollBar = function(){
+	self.setScrollBar = function(style){
 		if (store.settings.scrollBar === false){ UltimateLib.Logger.log("UltimateLib.VisualTweaks.scrollBar = false."); return; };
-		
 		var tweak = $('#visualTweaks');
-		tweak.append ("::-webkit-scrollbar { width: 12px; }");
-		tweak.append ("::-webkit-scrollbar-track-piece { width: 6px; }");
-		tweak.append ("::-webkit-scrollbar-track { width: 12px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.2);  border-radius: 8px; }");
-		tweak.append ("::-webkit-scrollbar-thumb { border-radius: 8px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.4);  background: radial-gradient(ellipse at center, rgba(250,198,149,1) 0%,rgba(245,171,102,1) 47%,rgba(239,141,49,1) 100%); }"); 
-		UltimateLib.Logger.log("UltimateLib.VisualTweaks.setScrollBar set."); 
+		switch (style){
+			case 1:
+				tweak.append ("::-webkit-scrollbar { width: 12px; }");
+				tweak.append ("::-webkit-scrollbar-track-piece { width: 6px; }");
+				tweak.append ("::-webkit-scrollbar-track { width: 12px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.2);  border-radius: 8px; }");
+				tweak.append ("::-webkit-scrollbar-thumb { border-radius: 8px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.4);  background: radial-gradient(ellipse at center, rgba(250,198,149,1) 0%,rgba(245,171,102,1) 47%,rgba(239,141,49,1) 100%); }"); 
+				UltimateLib.Logger.log("UltimateLib.VisualTweaks.setScrollBar set."); 
+			break;
+			default: 
+				tweak.append ("::-webkit-scrollbar { width: 12px; }");
+				tweak.append ("::-webkit-scrollbar-track-piece { width: 6px; }");
+				tweak.append ("::-webkit-scrollbar-track { width: 12px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.2);  border-radius: 8px; }");
+				tweak.append ("::-webkit-scrollbar-thumb { border-radius: 8px; -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.4);  background: radial-gradient(ellipse at center, rgba(250,198,149,1) 0%,rgba(245,171,102,1) 47%,rgba(239,141,49,1) 100%); }"); 
+				UltimateLib.Logger.log("UltimateLib.VisualTweaks.setScrollBar set."); 
+			break;
+		}
 	};
 	
     /**
      * @description Gives buttons a rounded edge.
      * @public
+     * @param {radius} Rounded edge radius on the button.
     */ 
-	self.setRoundedButtons = function () {
+	self.setRoundedButtons = function (radius) {
 		if (store.settings.roundedButtons === false){ UltimateLib.Logger.log("UltimateLib.VisualTweaks.roundedButtons = false."); return; };
+		if (!(radius >= 0)) { radius = 10;}
 		var tweak = $('#visualTweaks');
-		tweak.append ('.orangeButton, .deleteButton, .whiteButton, .selectorButton, .baseButton { border-radius: 10px; }');
+		tweak.append ('.orangeButton, .deleteButton, .whiteButton, .selectorButton, .baseButton, .contextMenuButton { border-radius: ' + radius + 'px; }');
 		UltimateLib.Logger.log("UltimateLib.VisualTweaks.setRoundedButtons set."); 
 	};
 	
