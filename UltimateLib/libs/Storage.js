8bd637e157614ee6d0b97ab4cfe637d910c714d3
abesco <alphabit@web.de>
2014-01-04 01:44:33 +0100

    Added Storage API
    This variant of a localStorage access, allows for correct saving and
    loading of global object data by using the same localStorage (HTML5)
    that GDT uses. This allows for perfect integration into game and tries
    to workaround the bug/problems related to it.

---
+++ UltimateLib/libs/Storage.js
@@ -0,0 +1,83 @@
+/**
+ * @fileOverview This is a custom storage class for the UltimateLib
+ * @version 1.0.0
+ * @author alphabit
+ * @constructor
+ * @augments UltimateLib
+ * @class Storage
+ */
+ UltimateLib.Storage = (function(self){
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Storage loading...");
+        
+    /**
+     * @description Initializes the module.
+     * @public
+    */ 
+    self.init = function(){
+        UltimateLib.Logger.log("UltimateLib.Storage init ran.");
+    };
+    
+    /**
+     * @description Writes the specified data to a storage object called UltimateLib.YOUR_STORAGE_ID
+     * @method write
+     * @param {string} storageId A custom unique ID for identifying your own storage section
+     * @param {object} A javascript object that contains the data to be stores in storage
+     * @returns {bool} True if writing to the storage was successful otherwise
+     * @public
+    */ 
+    self.write = function(storageId, data){
+        try {
+            if (isLocalStorageAvailable()){
+                window.localStorage['UltimateLib.Storage.'+storageId] = JSON.stringify(data);
+                UltimateLib.Logger.log("UltimateLib.Storage::write LocalStorage was successfully written at ID " + storageId);
+                return true;
+            }
+        }
+        catch(e) {
+            UltimateLib.Logger.log("UltimateLib.Storage::write An error occured trying to write to the local storage! Error: " + e.message);
+            return false;
+        }
+    };
+    
+    /**
+     * @description Reads from the UltimateLib localStorage the object stored under the specified ID
+     * @method read
+     * @param {string} storageId A custom unique ID for identifying your own storage section
+     * @returns {object} The object written in the localStorage and identified by the specified ID
+     * @public
+    */     
+    self.read = function(storageId){
+        try {
+            if ( isLocalStorageAvailable()){
+                UltimateLib.Logger.log("UltimateLib.Storage::read Trying to read from localStorage with ID " + storageId);
+                return JSON.parse(window.localStorage['UltimateLib.Storage.'+storageId]);
+            }
+        }
+        catch(e) {
+            UltimateLib.Logger.log("UltimateLib.Storage::read An error occured trying to read from the local storage! Error: " + e.message);
+            return false;
+        }
+    };
+    
+    /**
+     * @description Returns true if we have a local storage available on the client machine (using HTML5 localstorage feature)
+     * @method isLocalStorageAvailable
+     * @private
+    */     
+    function isLocalStorageAvailable() {
+        try {
+            return "localStorage" in window && window["localStorage"] !== null;
+        } 
+        catch (e) 
+        {
+            UltimateLib.Logger.log("UltimateLib.Storage could not find any local storage available! Error: " + e.message);
+            return false;
+        }
+    }
+        
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Storage loaded :-)");
+    
+    return self;
+})(UltimateLib.Storage || {});
