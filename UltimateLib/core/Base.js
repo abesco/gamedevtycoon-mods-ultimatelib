336c2d5d72846ae2547addf8d0a20ed5b0ffaa06
abesco <alphabit@web.de>
2014-01-11 10:00:21 +0100

    Fixed init error / Non existent folders

--- UltimateLib/core/Base.js
+++ UltimateLib/core/Base.js
@@ -1,7 +1,7 @@
 /**
  * @fileOverview UltimateLib base declaration. Makes the namespace and the base class available before any other thing.
  * @author alphabit
- * @version 1.0.0
+ * @version 1.0.1
  * @class UltimateLib
  * @description Ultimate provides access to library related functionality as well as access all the related libraries from here. This class hooks into GDT.
  * @constructor
@@ -16,20 +16,30 @@ var UltimateLib = (function(self) {
      * @public
     */            
     self.js             = [];
-
+      
     /**
      * @property {array} libraries
      * @description An array that contains all the core files of UltimateLib
      * @public
     */            
-    self.libraries      = [];
+    self.libraries      = [{name:'Configuration', file:''},
+                           {name:'Contracts', file:''}, 
+                           {name:'Dialog', file:''},
+                           {name:'Elements', file:''},
+                           {name:'NameGenerator', file:''},
+                           {name:'Notifications', file:'PopupMenu'},
+                           {name:'Publishers', file:''},
+                           {name:'Research', file:''},
+                           {name:'Storage', file:''},
+                           {name:'Utils', file:''},
+                           {name:'VisualTweaks', file:''}];
 
     /**
      * @property {array} libraries3rd
      * @description An array that contains all the 3rd party files of UltimateLib
      * @public
     */            
-    self.libraries3rd   = [];
+    self.libraries3rd   = [{name:'foswig', file:'Contracts'},{name:'jstorage', file:'Contracts'}];
 
     /**
      * @function getFiles
@@ -73,6 +83,32 @@ var UltimateLib = (function(self) {
      * @public
     */        
     self.init = function(){
+        var sections        = ['3rd','libs'];
+        self.js             = [];        
+        
+        var availMods = ModSupport.availableMods;
+        $.each(availMods, function(i, mod){
+            if (mod.id == 'UltimateLib'){
+                self.mod = mod;
+            }
+        });
+        
+        var handler = function (e) {
+            UltimateLib.Logger.log("A module has been loaded. " + e);
+            //custom code which will be called whenever one week passes.
+            // console.log("mod.loaded");
+            // console.log(ModSupport);
+        };
+        
+        GDT.on(GDT.eventKeys.mod.loaded, handler);        
+    };
+    
+    /**
+     * @function initDev
+     * @description Called for global initialization after Base.init()
+     * @public
+    */        
+    self.initDev = function(){
         var sections        = ['3rd','libs'];
         self.js             = [];
 
