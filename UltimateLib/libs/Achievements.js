720978a0687b00b6dd3b6bc11170a4ddf7db4ba3
Sir_Everard <deadlysin4@gmail.com>
2014-02-13 15:07:09 -0500

    Ultimate.Achievements Update

--- UltimateLib/libs/Achievements.js
+++ UltimateLib/libs/Achievements.js
@@ -6,7 +6,7 @@
  * @requires Base,Core,Logger
  * @since 1.3.0
  * @author Chad Keating (SirEverard)
- * @description Dialog is an UltimateLib library class providing dialog creation capabilities i.e. for use with PopupMenu.
+ * @description Achievements is an Ultimate Lib class intended to provide an API for adding achievements to the game.
  * @constructor
  * @param {Object} self An object representing the class itself or a new object for the purpose of extensibility. This parameter can be ignored.
  * @example 
@@ -31,24 +31,6 @@ UltimateLib.Achievements = (function (self) {
     // Show up in console
     UltimateLib.Logger.log("UltimateLib.Achievements loading...");
     
-    self.init = function () {
-        achievmentHook();
-        self.add(ultimate);
-    };
-    
-    function achievmentHook() {
-
-        var keepme = Achievements.getAllItems;
-
-        Achievements.getAllItems = function () {
-
-            var keep = keepme();
-            keep.addRange(self.collection);
-            return keep;
-
-        };
-    }
-
     /*
                 Previously used colours:
 
@@ -63,6 +45,111 @@ UltimateLib.Achievements = (function (self) {
                 green
     */
 
+    /**
+     * @method init 
+     * @description Initializes the module.
+    */
+    self.init = function () {
+        achievmentHook();
+        self.add(ultimate);
+    };
+
+    /**
+     * @method achievmentHook
+     * @description Hooks into Achievements.getAllItems and adds the custom achievements.
+     * @param {Object} options An object with acheivement options.
+     * @private 
+    */
+    function achievmentHook() {
+
+        var keepme = Achievements.getAllItems;
+
+        Achievements.getAllItems = function () {
+
+            var keep = keepme();
+            keep.addRange(self.collection);
+            return keep;
+
+        };
+    }
+
+
+    /**
+     * @method getTotalCount
+     * @description Returns the number of all achievements in the game.
+     * @return {Integer} Returns the number of all achievements in the game.
+    */
+    self.getTotalCount = function () {
+       return Achievements.getAllItems.length;
+    };
+
+    /**
+     * @method getCustomCount
+     * @description Returns the number of all custom achievements in the game.
+     * @return {Integer} Returns the number of all custom achievements in the game.
+    */
+    self.getCustomCount = function () {
+        return self.collection.length;
+    };
+
+
+    /**
+     * @method getAllItems
+     * @description Returns an array of all achievements in the game.
+     * @return {Array} Returns an array of all achievements in the game.
+    */
+    self.getAllItems = function () {
+        return Achievements.getAllItems;
+    };
+    /**
+     * @method getCustomItems
+     * @description Returns an array of all custom achievements in the game.
+     * @return {Array} Returns an array of all custom achievements in the game.
+    */
+    self.getCustomItems = function () {
+        return self.collection;
+    };
+
+
+    /**
+     * @method exists
+     * @description Checks to see if the given achievement id exsists in the game.
+     * @param {String} id An Id string relating to an achievement.
+     * @return {Boolean} True if id exists false otherwise.
+    */
+    self.exists = function (id) {
+        var exists = false;
+        $.grep(id, function (e, i) {
+            if (e.id === id) {
+                exists = true;
+            }
+        });
+        return exists;
+    };
+
+
+    /**
+     * @method get
+     * @description Returns the acheivement object relating to the
+     * @param {String} id An Id string relating to an achievement.
+     * @return {String} Returns achievement or "undefined".
+    */
+    self.get = function (id) {
+        var ach = $.grep(self.getAllItems(), function (e, i) {
+            return e.id === id;
+        });
+        if (ach.length < 1) {
+            ach = ["undefind"];
+        }
+        return ach[0];
+    };
+
+
+    /**
+     * @property {Object} ultimate
+     * @description An UltimateLib Achievement Object
+     * @private
+    */
     var ultimate = {
         id: "UltimateLib",
         title: "UltimateLib",
@@ -76,6 +163,26 @@ UltimateLib.Achievements = (function (self) {
         canEarnMultiple: false
     };
 
+    /**
+     * @method create
+     * @description Creates a achievement object with the parameters given via the options object
+     * @param {Object} options An object with acheivement options.
+     * @example 
+            Ultimate Achievements Example Object
+            {
+                    id: "UltimateLib",
+                    title: "UltimateLib",
+                    description: "Played a game with UltimateLib installed.",
+                    isAchieved: function (a) {
+                        return true;
+                    },
+                    tint: "#FFFFFF",
+                    value: 10,
+                    hidden: true,
+                    canEarnMultiple: false
+            };
+     * @return {Object} Returns an UL achievement object ready to be added. 
+    */
     self.create = function (options) {
         if (typeof Object.create !== 'function') {
             Object.create = function (o) {
@@ -91,25 +198,35 @@ UltimateLib.Achievements = (function (self) {
             that.isAchieved = options.isAchieved;
             that.tint = options.tint ? options.tint : "green";
             that.value = options.value ? options.value : 10;
-            that.hidden = options.hidden;
-            that.canEarnMultiple = options.canEarnMultiple;
+            that.hidden = options.hidden ? options.hidden : false;
+            that.canEarnMultiple = options.canEarnMultiple ? options.canEarnMultiple : false;
             that.toString = function () {
                 return 'UltimateLib.Achievements ' + that.title + ": " + that.description;
             };
             return that;
     };
 
+    /**
+     * @property {Array} collection
+     * @description An array that contains all custom UL Achievements.
+     * @public
+    */
     self.collection = [];
 
     /**
      * @method add
-     * @description Creates a dialog button and returns it as a wrapped jQuery element object for further usage
-     * @param {String} name 
+     * @description Adds an achievement object to the game.
+     * @param {Object} achievement 
     */
     self.add = function (achievement) {
 
         if (achievement instanceof UltimateLib.Achievements.create) {
-            achievement.id = "UltimateLibAchievment." + achievement.id;
+            achievement.id = "UltimateLibAchievement-" + achievement.id;
+            var i = 1;
+            while (self.exists(achievement.id)) {
+                achievement.id += "-" + i;
+                i++;
+            }
             self.collection.push(achievement);
         }
     };
