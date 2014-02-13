a6a547fcd6f386fd926611ee2a920dd915a25ae7
abesco <alphabit@web.de>
2014-02-14 00:19:11 +0100

    Skeleton for Graphs and Stats. Updated BuildList

---
+++ UltimateLib/libs/Graphs.js
@@ -0,0 +1,29 @@
+﻿/**
+ * @class Graphs
+ * @module UltimateLib
+ * @main UltimateLib
+ * @namespace UltimateLib
+ * @requires Base,Core,Logger
+ * @author Francesco Abbattista (alphabit)
+ * @description Graphs class providing various flot based graphical operations.
+ * @constructor
+ * @param {Object} self An object representing the class itself or a new object for the purpose of extensibility. This parameter can be ignored.
+ */
+
+UltimateLib.Graphs = (function (self) {
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Graphs loading...");
+
+    /**
+     * @method init
+     * @description Initializes the module.
+    */
+    self.init = function () {
+        UltimateLib.Logger.log("UltimateLib.Graphs init ran.");
+    };
+
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Graphs loaded :-)");
+
+    return self;
+})(UltimateLib.Graphs || {});
---
+++ UltimateLib/libs/Stats.js
@@ -0,0 +1,29 @@
+﻿/**
+ * @class Stats
+ * @module UltimateLib
+ * @main UltimateLib
+ * @namespace UltimateLib
+ * @requires Base,Core,Logger
+ * @author Francesco Abbattista (alphabit)
+ * @description Statistics class providing various statistical information about the game.
+ * @constructor
+ * @param {Object} self An object representing the class itself or a new object for the purpose of extensibility. This parameter can be ignored.
+ */ 
+ 
+UltimateLib.Stats = (function (self) {
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Stats loading...");
+
+    /**
+     * @method init
+     * @description Initializes the module.
+    */
+    self.init = function () {
+        UltimateLib.Logger.log("UltimateLib.Stats init ran.");
+    };
+
+    // Show up in console
+    UltimateLib.Logger.log("UltimateLib.Stats loaded :-)");
+
+    return self;
+})(UltimateLib.Stats || {});
